969d53d2996833cdbccac0adcf55858ef2e1e365
Gravitas Hunt <onagravitashunt@gmail.com>
2018-03-13 08:11:28 +0000

    Modified webpack build.assetsPublicPath to align with github.io's prefix

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/tbg-deployment-planner/',
 
     /**
      * Source Maps
