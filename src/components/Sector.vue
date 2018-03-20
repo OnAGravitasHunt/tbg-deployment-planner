7b9bd73212619f144bb2bd6fa8091b095ec3d8f6
Gravitas Hunt <onagravitashunt@gmail.com>
2018-03-20 11:51:52 +0000

    Added sector summaries, added secotrs collapsing

--- src/components/Sector.vue
+++ src/components/Sector.vue
@@ -1,7 +1,12 @@
 <template>
 <div class='sector' :id="'sector-' + name">
-  <h3>{{name}} &ndash; D{{sectorDef}}/{{def}}</h3>
-  <div class='sector-drag-wrapper'>
+  <div class='sector-header'>
+    <span class='sector-name' @click="toggleShow">{{name}}</span>
+    <span class='sector-info'>D{{sectorDef}}/{{def}}</span>
+    <span class='sector-info'>S{{sectorSci}}</span>
+    <span class='sector-info'>P{{sectorPre}}</span>
+  </div>
+  <div :class="`sector-drag-wrapper sector-show-${sectorShow}`">
     <draggable class='sector-drag'
         :id="'s-drag-' + index"
         v-model="sectorShips"
@@ -26,9 +31,20 @@ export default {
     Ship,
     draggable
   },
+  data () {
+    return {
+      sectorShow: true
+    }
+  },
   computed: {
     sectorDef () {
       return this.sectorShips.reduce((acc, curr) => acc + curr.classStats[5] + curr.bonusStats[5], 0)
+    },
+    sectorSci () {
+      return this.sectorShips.reduce((acc, curr) => acc + curr.classStats[1] + curr.veterancy + curr.bonusStats[1], 0)
+    },
+    sectorPre () {
+      return this.sectorShips.reduce((acc, curr) => acc + curr.classStats[4] + curr.veterancy + curr.bonusStats[4], 0)
     },
     sectorShips: {
       get () {
@@ -46,6 +62,9 @@ export default {
     onUpdate () {
       console.log('updating')
       this.$store.commit('sortSector', this.index)
+    },
+    toggleShow () {
+      this.sectorShow = !this.sectorShow
     }
   }
 }
@@ -53,7 +72,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .sector {
-  min-height: 100px;
+  /* min-height: 100px; */
   text-align: left;
   padding: 0 30px;
   margin-left: 250px;
@@ -67,5 +86,19 @@ export default {
 }
 .sector-drag {
   height: inherit;
+}
+.sector-header {
+  padding: 10px 0;
+}
+.sector-name {
+  font-size: 20px;
+  font-weight: bold;
+}
+.sector-info {
+  margin-left: 20px;
+}
+.sector-show-false {
+  display: none;
+  visibility: collapse;
 }
 </style>
